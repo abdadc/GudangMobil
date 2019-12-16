e33e1fa278cb68c425c0a7bbd9e17bd36b2d5ecc
navalinovian <viansatria57@gmail.com>
2019-12-16 15:45:11 +0700

    upload

--- app/src/main/java/com/example/gudangmobil/Activities/AddCarActivity.java
+++ app/src/main/java/com/example/gudangmobil/Activities/AddCarActivity.java
@@ -1,18 +1,115 @@
 package com.example.gudangmobil.Activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.gudangmobil.R;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class AddCarActivity extends AppCompatActivity {
+
+    Button choose, upload;
+    ImageView imgview;
+    StorageReference strRef;
+    public Uri imguri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_car);
+
+        strRef = FirebaseStorage.getInstance().getReference();
+
+        choose = findViewById(R.id.btn_choose);
+        upload = findViewById(R.id.btn_upload);
+        imgview = findViewById(R.id.img_preview);
+
+        choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FileChooser();
+            }
+        });
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FileUploader();
+            }
+        });
     }
 
+    private void FileChooser(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent ,1);
+    }
 
+    private void FileUploader(){
+        StorageReference reference = strRef.child(System.currentTimeMillis()+".");
+
+        final UploadTask uploadTask =  reference.putFile(imguri);
+                uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(AddCarActivity.this,"Image Uploaded Succesfully",Toast.LENGTH_SHORT).show();
+                    }
+//                    @Override
+//                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                        uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+//                            @Override
+//                            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+//                                if (!task.isSuccessful()) {
+//                                    throw task.getException();
+//
+//                                }
+//                                // Continue with the task to get the download URL
+//                                return strRef.getDownloadUrl()+getExtension(imguri);
+
+//                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                    }
+                });
+
+    }
+
+    private String getExtension(Uri uri){
+
+        ContentResolver cr = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(cr.getType(uri));
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==1 && resultCode == RESULT_OK & data != null ){
+            imguri = data.getData();
+            imgview.setImageURI(imguri);
+        }
+    }
 }
